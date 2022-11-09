177d9740e9d683bc4f12dc9510af52c0e40d84d1
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-08 22:14:49 -0300

    Create CourseController.java

---
+++ spring_crud/src/main/java/com/debora/spring_crud/controller/CourseController.java
@@ -0,0 +1,17 @@
+package com.debora.spring_crud.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    
+    @GetMapping
+    public List<Object> list() {
+        return null;
+    }
+}
