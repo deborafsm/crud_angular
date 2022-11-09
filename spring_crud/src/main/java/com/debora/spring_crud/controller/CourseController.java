a8e791ff3b592601d206fe29f769263457a6a9be
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-08 22:33:17 -0300

    Update CourseController.java

--- spring_crud/src/main/java/com/debora/spring_crud/controller/CourseController.java
+++ spring_crud/src/main/java/com/debora/spring_crud/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.debora.spring_crud.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,12 @@ import com.debora.spring_crud.repository.CourseRepository;
 @RequestMapping("/api/courses")
 public class CourseController {
     
+    @Autowired
     private CourseRepository courseRepository;
-
 
     @GetMapping
     public List<Object> list() {
         return null;
     }
+
 }
