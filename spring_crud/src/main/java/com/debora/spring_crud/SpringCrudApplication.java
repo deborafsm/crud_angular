d74a09ba524b5e87b0796e0f60e1d4ed469c566e
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-08 22:46:18 -0300

    create: FindAll() courses

--- spring_crud/src/main/java/com/debora/spring_crud/SpringCrudApplication.java
+++ spring_crud/src/main/java/com/debora/spring_crud/SpringCrudApplication.java
@@ -1,7 +1,12 @@
 package com.debora.spring_crud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.debora.spring_crud.model.Course;
+import com.debora.spring_crud.repository.CourseRepository;
 
 @SpringBootApplication
 public class SpringCrudApplication {
@@ -10,4 +15,16 @@ public class SpringCrudApplication {
 		SpringApplication.run(SpringCrudApplication.class, args);
 	}
 
+	@Bean
+	// Gerenciar todo o ciclo de vida;
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular");
+			c.setCategory("Front-end");
+			courseRepository.save(new Course());
+		};
+		
+	}
 }
--- spring_crud/src/main/java/com/debora/spring_crud/controller/CourseController.java
+++ spring_crud/src/main/java/com/debora/spring_crud/controller/CourseController.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.debora.spring_crud.model.Course;
 import com.debora.spring_crud.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
+// Cria Construtor
+@AllArgsConstructor
 public class CourseController {
-    
-    @Autowired
-    private CourseRepository courseRepository;
+
+    private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Object> list() {
-        return null;
+    public List<Course> list() {
+        return courseRepository.findAll();
     }
 
 }
