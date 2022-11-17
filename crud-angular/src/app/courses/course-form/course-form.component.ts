cbf85bb308b3faefd39bb88415a432f6180991e9
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-16 23:32:19 -0300

    create: formulary courses with snackbar
    https://material.angular.io/components/snack-bar/overview

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
@@ -8,13 +9,28 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CourseFormComponent implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
     });
   }
   onCancel() {}
-  onSubmit() {}
+
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result),
+      (error) => {
+        this.onError();
+      }
+    );
+  }
+  private onError() {
+    this._snackBar.open('Erro ao salvar', '', { duration: 3000 });
+  }
   ngOnInit(): void {}
 }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -10,6 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
@@ -22,7 +23,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSnackBarModule
   ],
 })
 export class AppMaterialModule {}
