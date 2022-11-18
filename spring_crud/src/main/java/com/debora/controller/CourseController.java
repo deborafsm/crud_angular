916c9c23db2e01d3c50d46111b2ba6baf337748f
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-17 22:08:23 -0300

    update backend: method create

--- spring_crud/src/main/java/com/debora/controller/CourseController.java
+++ spring_crud/src/main/java/com/debora/controller/CourseController.java
@@ -3,8 +3,11 @@ package com.debora.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.debora.model.Course;
@@ -30,8 +33,12 @@ public class CourseController {
 
     // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
-    public void create(@RequestBody Course course) {
-       
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        // .body(courseRepository.save(course));
     }
 
 }
