5d0936aefc00938f7d81c89a94647a22d876fd0f
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-17 22:15:12 -0300

    Update: OnSuccess() and OnCancell()

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
@@ -12,25 +13,33 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
     });
   }
-  onCancel() {}
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (result) => console.log(result),
+      (result) => this.onSuccess(),
+
       (error) => {
         this.onError();
       }
     );
   }
+  onCancel() {
+    this.location.back();
+  }
   private onError() {
     this._snackBar.open('Erro ao salvar', '', { duration: 3000 });
+  }
+  private onSuccess() {
+    this._snackBar.open('Sucesso ao salvar', '', { duration: 3000 });
+    this.onCancel();
   }
   ngOnInit(): void {}
 }
