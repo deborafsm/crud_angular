73b008c5b1e0db7124ca4f49b75e94ea65f88df0
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-18 22:51:41 -0300

    update: table

--- crud-angular/src/app/courses/courses-list/courses-list.component.ts
+++ crud-angular/src/app/courses/courses-list/courses-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.scss']
+  styleUrls: ['./courses-list.component.scss'],
 })
 export class CoursesListComponent implements OnInit {
+  displayedColumns = ['name', 'category', 'actions'];
+  @Input() courses: Course[] = [];
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
-
 }
