f29bbf17f5c84f8a9f0958824714834d2012283a
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-11-21 18:05:49 -0300

    Create: update course spring

--- spring_crud/src/main/java/com/debora/controller/CourseController.java
+++ spring_crud/src/main/java/com/debora/controller/CourseController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,14 @@ public class CourseController {
         return courseRepository.save(course);
         // return ResponseEntity.status(HttpStatus.CREATED)
         // .body(courseRepository.save(course));
+    }
+
+    // Buscando Curso por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
