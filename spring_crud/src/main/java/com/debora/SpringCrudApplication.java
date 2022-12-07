d6fe24a1c712e70c0aea291847f567f5c2341ae1
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2022-12-07 00:08:44 -0300

    Update SpringCrudApplication.java

--- spring_crud/src/main/java/com/debora/SpringCrudApplication.java
+++ spring_crud/src/main/java/com/debora/SpringCrudApplication.java
@@ -15,20 +15,20 @@ public class SpringCrudApplication {
 		SpringApplication.run(SpringCrudApplication.class, args);
 	}
 
-	@Bean
-	Gerenciar todo o ciclo de vida;
-	CommandLineRunner initDatabase(CourseRepository courseRepository) {
-		return args -> {
-			courseRepository.deleteAll();
-			Course c = new Course();
-			c.setName("Angular");
-			c.setCategory("Front-end");
-			Course c1 = new Course();
-			c1.setName("Spring");
-			c1.setCategory("Back-end");
-			courseRepository.save(c);
-			courseRepository.save(c1);
-		};
+	// @Bean
+	// Gerenciar todo o ciclo de vida;
+	// CommandLineRunner initDatabase(CourseRepository courseRepository) {
+	// 	return args -> {
+	// 		courseRepository.deleteAll();
+	// 		Course c = new Course();
+	// 		c.setName("Angular");
+	// 		c.setCategory("Front-end");
+	// 		Course c1 = new Course();
+	// 		c1.setName("Spring");
+	// 		c1.setCategory("Back-end");
+	// 		courseRepository.save(c);
+	// 		courseRepository.save(c1);
+	// 	};
 
-	}
+	// }
 }
